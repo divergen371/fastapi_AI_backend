9f62e436920e9c39af1712b8acbcb184d496d964
divergen371 <51857554+divergen371@users.noreply.github.com>
2023-06-25 12:59:00 +0000

    :sparkles: 生存確率を予測する。 モデルの学習と保存、与えられた特徴量について学習済みのモデルから生存確率を算出する。

--- api/machine_leaning/titanic.py
+++ api/machine_leaning/titanic.py
@@ -71,3 +71,45 @@ class GenerateModel:
         preprocessed_df = cls.__preprocess_df(df)
         lgbm_model = cls.__train_model(preprocessed_df)
         cls.__save_model(lgbm_model)
+
+
+class PredictOnAPI(GenerateModel):
+    def __init__(self):
+        pass
+
+    @classmethod
+    def __load_model(cls):
+        """pickle形式で作成されたモデルを読み込む。存在しない場合は作成する。"""
+        path_to_model = cls.base_dir / "model" / "model.pkl"
+        if path_to_model.exists() == False:
+            print("modelが存在しないので作成します。")
+            cls.generate_model()
+        with open(path_to_model, "rb") as f:
+            model = pickle.load(f)
+
+        return model
+
+    @staticmethod
+    def __encode_pclass(x: str):
+        if x == "上層クラス（お金持ち）":
+            return 1
+        elif x == "中級クラス（一般階級）":
+            return 2
+        elif x == "下層クラス（労働階級）":
+            return 3
+        else:
+            return np.nan
+
+    @classmethod
+    def derive_survival_probablity(
+        cls, Sex: str, Pclass: str, Age: int, Parch: int, SibSp: int
+    ) -> float:
+        """与えられた特徴量について、事前に学習済みのモデルを用いてタイタニック生存確率を算出する。"""
+        model = cls.__load_model()
+
+        encode_sex = cls.encode_sex(Sex)
+        encode__pclass = cls.__encode_pclass(Pclass)
+        features = np.array([[encode_sex, encode__pclass, Age, Parch, SibSp]])
+        survival_probability = model.predict_proba(features)[0][1]
+
+        return round(survival_probability, 3)
